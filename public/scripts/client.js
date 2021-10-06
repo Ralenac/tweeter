f37fd5a306a7b6b30891c19a214aedf0140f0c89
Ralenac <t.ralenac@gmail.com>
2021-10-06 18:34:49 -0400

    Fetch tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,48 +1,49 @@
 $(document).ready(() => {
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd"
+  //     },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
+
+  const renderTweets = function (tweets) {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet)
+      $('#tweets-container').prepend($tweet);
+    }
+
   }
-]
-
-const renderTweets = function(tweets) {
-  for (const tweet of tweets) {
-    const $tweet = createTweetElement(tweet)
-    $('#tweets-container').prepend($tweet); 
-  }
-  
-}
 
 
-const createTweetElement = ((tweet) => {
-  const avatar = tweet.user.avatars;
-  const name = tweet.user.name;
-  const username = tweet.user.handle;
-  const tweetContent = tweet.content.text;
-  const time = timeago.format(new Date())
+  const createTweetElement = ((tweet) => {
+    const avatar = tweet.user.avatars;
+    const name = tweet.user.name;
+    const username = tweet.user.handle;
+    const tweetContent = tweet.content.text;
+    const time = timeago.format(new Date())
 
 
-  const html = `<article class="tweet">
+    const html = `<article class="tweet">
   <header>
     <div class="client"><img src=${avatar} alt="" ><p>${name}</p></div>
        
@@ -59,25 +60,45 @@ const createTweetElement = ((tweet) => {
       </div>
   </footer>
 </article>`
-return html;
+    return html;
 
-})
-
-
-renderTweets(data);
-
-
-$("#formTweet").submit(function(event) {
-  event.preventDefault()
-  const tweet = $("#tweet-text").serialize();
-  $.ajax({
-    method: "POST",
-    url: "/tweets",
-    data: tweet
   })
-  console.log(tweet)
-});
 
+
+  // renderTweets(data);
+
+
+  $("#formTweet").submit(function(event) {
+    event.preventDefault()
+    const tweet = $("#tweet-text").serialize();
+    $.ajax({
+      method: "POST",
+      url: "/tweets",
+      data: tweet
+    })
+    console.log(tweet)
+  });
+
+
+  const loadTweets = function() {
+    
+    $.ajax({
+      method: "GET",
+      url: "/tweets",
+      success: (tweets) => {
+        console.log("data", tweets)
+        renderTweets(tweets)
+      },
+      error: (err) => {
+      console.error(`there was an error: ${err}`);
+      }
+    })
+    
+    
+
+  }
+
+  loadTweets();
 
 })
 
